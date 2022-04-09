69ce1a9df3d6e7c1dd2e1194f57ad6e09c0fc9d5
zeromountain <sonyeongsan@gmail.com>
2022-04-09 16:53:14 +0900

    옵저버 패턴 미완

--- src/__tests__/reactive.spec.js
+++ src/__tests__/reactive.spec.js
@@ -1,16 +1,16 @@
-import { observable, observe } from "../reactive";
+import { observable, observe } from '../reactive';
 
-async function wait (time = 1000 / 60) {
+async function wait(time = 1000 / 60) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-describe("reactive test", () => {
-  it("observable로 만든 객체가 observe 내에서 사용될 경우", async () => {
+describe('reactive test', () => {
+  it('observable로 만든 객체가 observe 내에서 사용될 경우', async () => {
     const state = observable({ a: 1, b: 2 });
 
     let computed = '';
     function compute() {
-       computed = `a + b = ${state.a + state.b}`;
+      computed = `a + b = ${state.a + state.b}`;
     }
 
     observe(compute);
@@ -27,8 +27,9 @@ describe("reactive test", () => {
     expect(computed).toBe(`a + b = 30`);
   });
 
-  it("똑같은 값을 할당할 경우, 실행하지 않음", async () => {
-    const state = observable({ a: 1, b: 2 });
+  it('똑같은 값을 할당할 경우, 실행하지 않음', async () => {
+    const { state } = observable({ a: 1, b: 2 });
+    console.log(state);
 
     let computed = '';
     let callCount = 0;
@@ -53,20 +54,19 @@ describe("reactive test", () => {
     expect(computed).toBe(`a + b = 30`);
     expect(callCount).toBe(2);
 
-
     await wait();
     state.a = 10;
     expect(computed).toBe(`a + b = 30`);
     expect(callCount).toBe(2);
   });
 
-  it("똑같은 값을 할당할 경우, 실행하지 않음 - 얕은비교(배열)", async () => {
+  it('똑같은 값을 할당할 경우, 실행하지 않음 - 얕은비교(배열)', async () => {
     const state = observable({ a: 1, b: 2, c: [3, 4] });
 
     let computed = '';
     let callCount = 0;
     function compute() {
-      const [ c, d ] = state.c;
+      const [c, d] = state.c;
       computed = `a + b + c + d = ${state.a + state.b + c + d}`;
       callCount += 1;
     }
@@ -82,7 +82,7 @@ describe("reactive test", () => {
     expect(callCount).toBe(0);
   });
 
-  it("똑같은 값을 할당할 경우, 실행하지 않음 - 얕은비교(객체)", async () => {
+  it('똑같은 값을 할당할 경우, 실행하지 않음 - 얕은비교(객체)', async () => {
     const state = observable({ a: 1, b: 2, child: { c: 3, d: 4 } });
 
     let computed = '';
@@ -98,14 +98,15 @@ describe("reactive test", () => {
     expect(computed).toBe(`a + b + c + d = 10`);
     expect(callCount).toBe(0);
 
-    state.child = { c: 3, d: 4 }
+    state.child = { c: 3, d: 4 };
 
     expect(computed).toBe(`a + b + c + d = 10`);
     expect(callCount).toBe(0);
   });
 
-  it("마지막으로 할당한 값에 대해서만 observe를 실행함", () => {
+  it('마지막으로 할당한 값에 대해서만 observe를 실행함', () => {
     const state = observable({ a: 1, b: 2 });
+    console.log(state);
 
     let computed = '';
     let callCount = 0;
@@ -132,4 +133,4 @@ describe("reactive test", () => {
     expect(computed).toBe(`a + b = 12`);
     expect(callCount).toBe(1);
   });
-})
+});
--- src/reactive.js
+++ src/reactive.js
@@ -1,7 +1,17 @@
-export function observe() {
+// subscriber
+export function observe() {}
 
-}
-
-export function observable() {
-
+// publish
+export function observable(initialState) {
+  // 탐색 목록, state, 구독관리, 구독알림
+  const observers = [];
+  return {
+    state: initialState || null,
+    subscribe: (listener) => observers.push(listener) - 1,
+    unsubscribe: (idx) => observers.splice(idx, 1),
+    notify: (newState) => {
+      this.state = newState;
+      observers.forEach((observer) => observer(newState));
+    },
+  };
 }
